cc09361a7c1a8f5e064549cf1427dbd56c41f338
Tung Anh Vu <vu.tunganh96@gmail.com>
2018-03-14 16:41:27 +0100

    reworked so that Point struct isn't needed anymore

--- 3d-version/3d-version.jl
+++ 3d-version/3d-version.jl
@@ -4,82 +4,113 @@
 # The primary purpose of this program is to find all mistakes, that will
 # happen in the real program and solve them early.
 
-include("point.jl")
 include("polynomial-generator.jl")
 
 Base.__precompile__()
 module MWLS3D
-using Point3D
 using PolynomialGenerator
-using ArgParse
 using DataFrames
 using CSV
 using DynamicPolynomials
 using MultivariatePolynomials
 
-export wls, parseInputPoints, wlsObject
+export wls, parseInputPoints, WlsObject, Point
 
-"Reads the input and returns an array of points"
+const Point = Vector{Float64}
+
+"""
+reads an input csv file
+
+returns input "points" and their respective outputs
+"""
 function parseInputPoints(inputFilename::String)
   parsedInput = CSV.read(inputFilename)
-  pointCount::Int64 = size(parsedInput, 1)
-  dimensionCount::Int64 = size(parsedInput, 2)
-  toReturn::Vector{Point} = []
+  pointCount::Int = size(parsedInput, 1)
+  dimensions::Int = size(parsedInput, 2)
+  inputs::Vector{Point} = []
+  inputDimension::Int = dimensions - 1
+  outputs::Vector{Float64} = []
   for i in 1:pointCount
     p = parsedInput[i, :]
-    apt = [p[1, j] for j in 1:dimensionCount]
-    push!(toReturn, Point(apt))
+    ipt = [p[1, j] for j in 1:inputDimension]
+    push!(inputs, ipt)
+    push!(outputs, p[1, dimensions])
   end
-  return toReturn
+  return inputs, outputs
 end
 
-function dd(p::Point, q::Point)
-  sqrt((p.x - q.x)^2 + (p.y - q.y)^2)
-end
-
+"""
+# Attributes
+- `vars::Vector{PolyVar{true}}`: variables of the polynomial, these are constructed automatically if constructed by the `wls` function
+- `b::Vector{Monomial{true}}`: basis of the polynomial, these are constructed automatically if constructed by the `wls` function
+- `inputs::Vector{Point}`: the vector of input points
+- `outputs::Vector{Float64}`: the vector of output scalars
+- `inputDimension::Int`: dimension of each input in `inputs`, this is constructed automatically if constructed by the `wls` function
+- `EPS::Float64`: ε of the method
+- `weightFunction::Function`: weighting function of the method
+- `matrix`: the result of `b * transpose(b)`, this is constructed automatically if constructed by the `wls` function
+"""
 struct WlsObject
   vars::Vector{PolyVar{true}}
-  b::Array{Monomial{true}}
-  data::Vector{Point}
+  b::Vector{Monomial{true}}
+  inputs::Vector{Point}
+  outputs::Vector{Float64}
+  inputDimension::Int
   EPS::Float64
-  wfun
-  basis
+  weightFunction
+  matrix
 end
 
-function WlsObject(vars, b, data, EPS, wfun)
-  WlsObject(vars, b, data, EPS, wfun, b * transpose(b))
+function WlsObject(vars, b, inputs, outputs, EPS, wfun)
+  WlsObject(vars, b, inputs, outputs, length(inputs[1]) - 1, EPS, wfun, b * transpose(b))
 end
 
 """
-this function approximates the WlsObject at inputPoint
+    `(wo::WlsObject)(inputPoint::Point, value::Bool)`
+
+Approximates the `WlsObject` at `inputPoint`.
+If `value` is true, the approximated value will be outputted instead of the coefficients.
 """
-function (w::WlsObject)(inputPoint::Point)
-  m = length(w.b)
+function (obj::WlsObject)(inPt::Point, value::Bool)
+  m = length(obj.b)
+  datalen = length(obj.inputs)
   firstTerm = zeros(m, m)
   secondTerm = zeros(m)
 
-  for p in w.data
-    dist = w.wfun(dd(inputPoint, p), w.EPS)
+  for p in 1:datalen
+    curPt = obj.inputs[p]
+    w = obj.weightFunction(norm(inPt - curPt), obj.EPS)
     for i in 1:m
       for j in 1:m
-        firstTerm[i, j] += dist * subs(w.basis[i, j], w.vars => (p.x, p.y))
+        firstTerm[i, j] += w * obj.matrix[i, j](obj.vars => curPt)
       end
-      secondTerm[i] += dist * subs(w.b[i], w.vars => (p.x, p.y)) * p.z
+      secondTerm[i] += w * obj.b[i](obj.vars => curPt) * obj.outputs[p]
     end
   end
 
-  result = firstTerm \ secondTerm
+  coefficients = firstTerm \ secondTerm
+  if value
+    poly = sum(coefficients .* obj.b)
+    return poly(obj.vars => inPt)
+  else
+    return coefficients
+  end
 end
 
 """
-this function creates a wls object
+    wls(inputs::Vector{Point}, outputs::Vector{Float64}, maxDegree::Int64, EPS::Float64, wfun::Function)
 
-wfun is a weighting function, that should be used
-it should take one parameter
+# Arguments
+- `inputs::Vector{Point}`: a vector of input points,
+- `outputs::Vector{Point}`: a vector of output points,
+- `maxDegree::Int64`: the maximal degree of each polynomial term in the method,
+- `EPS::Float64`: ε of the method,
+- `wfun::Function`: wfun should be in form `wfun(distance::Float64, EPS::Float64)`.
 """
-function wls(samplePoints::Vector{Point}, variableCount::Int64, dimensionCount::Int64, EPS::Float64, wfun)
-  vars::Vector{PolyVar{true}}, b::Array{Monomial{true}} = gen(variableCount, dimensionCount)
-  return WlsObject(vars, b, samplePoints, EPS, wfun)
+function wls(inputs::Vector{Point}, outputs::Vector{Float64}, maxDegree::Int64, EPS::Float64, wfun::Function)
+  inputDimension = length(inputs[1])
+  vars::Vector{PolyVar{true}}, b::Vector{Monomial{true}} = gen(inputDimension, maxDegree)
+  return WlsObject(vars, b, inputs, outputs, EPS, wfun)
 end
 end
 
