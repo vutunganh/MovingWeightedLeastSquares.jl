6236382416f6cfe689040d71a2604ba6fcfe0e0e
Tung Anh Vu <vu.tunganh96@gmail.com>
2018-03-23 23:21:41 +0100

    made wls more similar to mwls aesthetically

--- src/wls-object.jl
+++ src/wls-object.jl
@@ -1,40 +1,44 @@
 """
-# Attributes
-- `vars::Vector{PolyVar{true}}`: variables of the polynomial, these are constructed automatically if constructed by the `wls` function
-- `b::Vector{Monomial{true}}`: basis of the polynomial, these are constructed automatically if constructed by the `wls` function
+# User provided attributes
 - `inputs`: the vector or array view of input points
 - `outputs`: the vector or array view of output scalars
 - `EPS::Float64`: ε of the method
 - `weightFunction::Function`: weighting function of the method
-- `matrix`: the result of `b * transpose(b)`, this is constructed automatically if constructed by the `wls` function
+
+# Automatically created attributes
+If created by the `wls` function, the attributes in this section are created automatically.
+
+- `vars::Vector{PolyVar{true}}`: variables of the polynomial,
+- `b::Vector{Monomial{true}}`: basis of the polynomial,
+- `matrix`: the result of `b * transpose(b)`.
 """
 struct WlsObject
-  vars::Vector{PolyVar{true}}
-  b::Vector{Monomial{true}}
   inputs
   outputs
   EPS::Float64
-  weightFunction
+  weightFunc::Function
+  vars::Vector{PolyVar{true}}
+  b::Vector{Monomial{true}}
   matrix
 end
 
-function WlsObject(vars, b, inputs, outputs, EPS, wfun)
-  WlsObject(vars, b, inputs, outputs, EPS, wfun, b * transpose(b))
+function WlsObject(inputs, outputs, EPS, weightFunc, vars, b)
+  WlsObject(inputs, outputs, EPS, weightFunc, vars, b, b * transpose(b))
 end
 
 """
-    wls(inputs::Vector{Point}, outputs::Vector{Float64}, maxDegree::Int64, EPS::Float64, wfun::Function)
+    wls(inputs::Array{Float64, 2}, outputs::Vector{Float64}, maxDegree::Int, EPS::Float64, weightFunc::Function)
 
 # Arguments
 - `inputs`: a 2d array where each point is on a single row,
 - `outputs`: an array of output scalars,
 - `maxDegree::Int64`: the maximal degree of each polynomial term in the method,
 - `EPS::Float64`: ε of the method,
-- `wfun::Function`: wfun should be in form `wfun(distance::Float64, EPS::Float64)`.
+- `weightFunc::Function`: weighting function of the method. It should be in form of `(distance, EPS) -> Float64`.
 """
-function wls(inputs, outputs, maxDegree::Int64, EPS::Float64, wfun::Function)
+function wls(inputs, outputs, maxDegree::Int, EPS::Float64, weightFunc::Function)
   inputDim = size(inputs, 2)
   vars::Vector{PolyVar{true}}, b::Vector{Monomial{true}} = generateMonomials(inputDim, maxDegree)
-  return WlsObject(vars, b, inputs, outputs, EPS, wfun)
+  return WlsObject(inputs, outputs, EPS, weightFunc, vars, b)
 end
 
--- src/wls-ops.jl
+++ src/wls-ops.jl
@@ -1,17 +1,11 @@
-"""
-    `(wo::WlsObject)(inputPoint::Point, value::Bool)`
-
-Approximates the `WlsObject` at `inputPoint`.
-If `value` is true, the approximated value will be outputted instead of the coefficients.
-"""
-function (obj::WlsObject)(inPt::Point, value::Bool)
+function calcWlsCoefficients(obj::WlsObject, inPt::Point)
   m = length(obj.b)
   datalen = size(obj.inputs, 1)
   firstTerm = zeros(m, m)
   secondTerm = zeros(m)
 
   for p in 1:datalen
-    curPt = view(obj.inputs, p, :)
+    curPt = obj.inputs[p, :]
     w = obj.weightFunction(norm(inPt - curPt), obj.EPS)
     for i in 1:m
       for j in 1:m
@@ -22,12 +16,19 @@ function (obj::WlsObject)(inPt::Point, value::Bool)
   end
 
   coefficients = firstTerm \ secondTerm
-  if value
-    poly = polynomial(coefficients, obj.b)
-    return poly(obj.vars => inPt)
-  else
-    return coefficients
-  end
+  return coefficients
+end
+
+"""
+    `(wo::WlsObject)(inputPoint::Point, value::Bool)`
+
+Approximates the `WlsObject` at `inputPoint`.
+If `value` is true, the approximated value will be outputted instead of the coefficients.
+"""
+function (obj::WlsObject)(inPt::Point)
+  c = calcWlsCoefficients(obj, inPt)
+  poly = polynomial(coefficients, obj.b)
+  return poly(obj.vars => inPt)
 end
 
 
