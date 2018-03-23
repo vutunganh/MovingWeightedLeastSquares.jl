bc6e3ab4b133025c7d2775d4149f3a15edd17eda
Tung Anh Vu <vu.tunganh96@gmail.com>
2018-03-23 23:11:34 +0100

    some small changes to sample generator

--- src/sample-generator.jl
+++ src/sample-generator.jl
@@ -1,50 +1,40 @@
-function randUniform(a, b)
-  return a + rand() * (b - a)
-end
-
 """
     `generatePoint(dims::Int, range::AbstractFloat, offset::AbstractFloat = 0)`
 
 Generates a random `dims`-dimensional point represented as an array.
 
-# Arguments
-- `range`: specifies symmetric bounds for each dimension of the generated point,
-- `offset`: offsets the center of `range` interval.
+d ∈ (offset - range / 2, offset + range / 2) for each dimension of the point.
 """
 function generatePoint(dims::Int, range::AbstractFloat, offset::AbstractFloat = 0.0)
   return range * rand(dims) - range / 2 + offset 
 end
 
-"Generates `n` random points without outputs. The rest of parameters are same as arguments for `generatePoint`."
+"""
+Generates `n` random points without outputs. The rest of parameters are same as parameters for `generatePoint`.
+"""
 function generatePoints(n::Int, dims::Int, range::AbstractFloat, offset::AbstractFloat = 0.0)
   return [generatePoint(dims, range, offset) for i in 1:n]
 end
 
-function writeToCsv(data::Vector{Vector{Float64}}, output::String)
-  for i in 1:(length(data) - 1)
-    if length(data[i]) != length(data[i + 1])
-      error("Array dimension mismatch on row $i")
-    end
-  end
-
-  df = DataFrame()
-  for i in 1:(length(data[1]) - 1)
-    df[Symbol("x", i)] = [d[i] for d in data]
-  end
-  df[Symbol("y")] = [d[length(data[1])] for d in data]
-  CSV.write(output, df)
-end
-
+"""
+# Arguments
+- `amount`: the amount of points to be generated,
+- `polyDegree`: the degree of polynomial used for data generation,
+- `polyDim`: the spatial dimension of each term in the polynomial used for data generation,
+- `ptRange` and `ptOffset`: the same as `generatePoint`,
+- `polyRange` and `polyOffset`: the same as `generatePoint`, but for each term in the polynomial used for data generation,
+- `outputFile`: name of the output file
+"""
 function generateRandomData(amount::Int, polyDegree::Int, polyDim::Int, ptRange::Float64, ptOffset::Float64, polyRange::Float64, polyOffset::Float64, outputFile::String)
   vars, mons = generateMonomials(polyDegree, polyDim)
   coeffs = polyRange * rand(length(mons)) - (polyRange / 2 + polyOffset)
   poly = polynomial(coeffs, mons)
-  res = generatePoints(amount, ptDegree, ptRange, ptOffset)
+  res = generatePoints(amount, polyDegree, ptRange, ptOffset)
   for p in res
     push!(p, poly(vars => p))
   end
-  writeToCsv(res, outputFile)
-  println("Polynomial '$pal' was used for data generation")
+  writecsv(outputFile, res)
+  println("Polynomial '$poly' was used for data generation")
   println("Done")
 end
 
