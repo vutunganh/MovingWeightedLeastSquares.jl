29f659d0dcef2718971347e20d00e550651e90f4
Tung Anh Vu <vu.tunganh96@gmail.com>
2018-03-26 21:26:29 +0200

    WIP refactoring

--- src/mwls-ops.jl
+++ src/mwls-ops.jl
@@ -30,7 +30,7 @@ function calcMwlsCoefficients(obj::MwlsObject, inPt::Point, dist::Float64)
 end
 
 """
-    `(mo::MwlsObject)(inputPoint::Point, dist::Float64)`
+    `(obj::MwlsObject)(inputPoint::Point, dist::Float64)`
 
 Approximates the `MwlsObject` at `inputPoint`.
 `Dist` determines the method's distance threshold around `inPt`.
@@ -42,4 +42,42 @@ function (obj::MwlsObject)(inPt::Point, dist::Float64)
   c = calcMwlsCoefficients(obj, inPt, dist)
   poly = polynomial(c, obj.b)
   return poly(obj.vars => inPt)
+end
+
+"""
+    `calcDiffMwlsSingle(obj::MwlsObject, pt::Point, dir::Int, times::Int; dist = obj.EPS)`
+
+Calculates `obj`'s approximated polynomial's `times`-th derivative by `var`-th variable.
+"""
+function calcDiffMwlsSingle(obj::MwlsObject, pt::Point, var::Int, times::Int; dist = obj.EPS)
+  c = calcMwlsCoefficients(obj, pt, dist)
+  poly = polynomial(c, obj.b)
+  return differentiate(poly, obj.vars[var], times)
+end
+
+"""
+    `calcDiffMwlsPolys(obj::MwlsObject, pt::Point, dirs::Tuple; dist = obj.EPS)`
+
+Calculates 
+"""
+function calcDiffMwlsPolys(obj::MwlsObject, pt::Point, dirs; dist = obj.EPS)
+  if length(dirs) != size(obj.inputs, 2)
+    error("Dimension mismatch")
+  end
+  c = calcMwlsCoefficients(obj, pt, dist)
+  poly = polynomial(c, obj.b)
+  return [differentiate(poly, obj.vars[i], dirs[i]) for i in 1:length(obj.vars)]
+end
+
+function diff(obj::MwlsObject, pt::Point, dirs::Int; dist = obj.EPS)
+  diff(obj, pt, (dirs, 0); dist = dist)
+end
+
+function diff(obj::MwlsObject, pt::Point, dirs::Tuple; dist = obj.EPS)
+  if size(pt, 1) == 1
+    pt = [pt; 0]
+  end
+  pl = calcDiffMwlsPolys(obj, pt, dirs; dist = dist)
+  println(pl)
+  return [p(obj.vars => pt) for p in pl]
 end
