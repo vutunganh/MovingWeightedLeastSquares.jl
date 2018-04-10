de4e90c0489cd86ffa735413dd13d6a3b2c111d2
Tung Anh Vu <vu.tunganh96@gmail.com>
2018-04-10 16:06:32 +0200

    started moving tests to runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,2 +1,89 @@
+using MovingWeightedLeastSquares
+using Base.Random
 using Base.Test
-@test true == true
+using DynamicPolynomials, MultivariatePolynomials
+
+include("sample-generator.jl")
+
+function randRange(mean::Real, width::Real)
+  return 
+end
+
+@testset "1d approximation" begin
+  xs = [j * 0.1 for j in 0:100]
+  fs = [sin(x) for x in xs]
+
+  m = mwlsKd(xs, fs, 1, (d, e) -> exp(-d^2))
+  cll = mwlsCll(xs, fs, 1, (d, e) -> exp(-d^2))
+
+  testStatus = true
+  for p in 0:0.1:10
+    testStatus &= isapprox(m(p), sin(p), atol=0.1)
+  end
+  @test testStatus == true
+
+  testStatus = true
+  for p in 0:0.1:10
+    testStatus &= isapprox(cll(p), sin(p), atol=0.1)
+  end
+  @test testStatus == true
+
+  # not enough data for approximation, zero returned by default
+  @test isapprox(m(-0.9), 0, atol=0.1)
+  @test isapprox(cll(-0.9), 0, atol=0.1)
+  @test isapprox(m(10.9), 0, atol=0.1)
+  @test isapprox(cll(10.9), 0, atol=0.1)
+end
+
+@testset "single table data" begin
+  xs = [j * 0.1 for j in 0:100]
+  fs = [sin(x) for x in xs]
+  data = hcat(xs, fs)
+
+  m = mwlsKd(data, 1, (d, e) -> exp(-d^2))
+  cll = mwlsCll(data, 1, (d, e) -> exp(-d^2))
+
+  testStatus = true
+  for p in 0:0.1:10
+    testStatus &= isapprox(m(p), sin(p), atol=0.1)
+  end
+  @test testStatus == true
+
+  testStatus = true
+  for p in 0:0.1:10
+    testStatus &= isapprox(cll(p), sin(p), atol=0.1)
+  end
+  @test testStatus == true
+
+  # not enough data for approximation, zero returned by default
+  @test isapprox(m(-0.9), 0, atol=0.1)
+  @test isapprox(cll(-0.9), 0, atol=0.1)
+  @test isapprox(m(10.9), 0, atol=0.1)
+  @test isapprox(cll(10.9), 0, atol=0.1)
+end
+
+@testset "1d derivative" begin
+  xs = [j * 0.1 for j in 0:100]
+  fs = [sin(x) for x in xs]
+
+  m = mwlsKd(xs, fs, 1, (d, e) -> exp(-d^2))
+  cll = mwlsCll(xs, fs, 1, (d, e) -> exp(-d^2))
+
+  testStatus = true
+  for p in 0:0.1:10
+    isapprox(mwlsDiff(m, p, 1), cos(p), atol=0.1)
+  end
+  @test testStatus == true
+
+  testStatus = true
+  for p in 0:0.1:10
+    testStatus &= isapprox(mwlsDiff(cll, p, 1), cos(p), rtol=0.1)
+  end
+  @test testStatus == true
+
+  # not enough data for approximation, zero returned by default
+  @test isapprox(mwlsDiff(m, -0.9, 1), 0, atol=0.1)
+  @test isapprox(mwlsDiff(cll, -0.9, 1), 0, atol=0.1)
+  @test isapprox(mwlsDiff(m, 10.9, 1), 0, atol=0.1)
+  @test isapprox(mwlsDiff(cll, 10.9, 1), 0, atol=0.1)
+end
--- test/single-derivative.jl
+++
@@ -1,8 +0,0 @@
-using MovingWeightedLeastSquares
-
-xs = [j * 0.1 for j in 0:100]
-fs = [sin(x) for x in xs]
-
-m = mwls(xs, fs, 1., (d, e) -> exp(-d^2))
-
-println(MovingWeightedLeastSquares.diff(m, [0.5], (1)))
