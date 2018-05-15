214f3ac9a08393b78af752ca0fe270f7b3a2c6bb
Tung Anh Vu <vu.tunganh96@gmail.com>
2018-05-16 00:31:18 +0200

    removal of gitlab stuff

--- coverage_script.jl
+++
@@ -1,6 +0,0 @@
-Pkg.add("Coverage")
-cd(Pkg.dir("MovingWeightedLeastSquares"))
-using Coverage
-covered_lines, total_lines = get_summary(process_folder())
-percentage = covered_lines / total_lines * 100
-println("($(percentage)%) covered")
