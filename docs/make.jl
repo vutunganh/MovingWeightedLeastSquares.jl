5de57ae89755340ba0362dcb0302c8eacbb5a86c
Tung Anh Vu <vu.tunganh96@gmail.com>
2018-05-23 21:35:53 +0200

    another attempt at fixing docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,17 +1,22 @@
 using Documenter, MovingWeightedLeastSquares
 
-makedocs(modules = [MovingWeightedLeastSquares],
-         format = :html,
-         sitename ="MovingWeightedLeastSquares.jl",
-         authors = "Tung Anh Vu",
-         pages = Any[
-           "Home" => "index.md",
-           "Constructors" => "constructors.md",
-           "Approximation" => "approximation.md"
-         ]
-        )
+makedocs(
+  modules = [MovingWeightedLeastSquares],
+  format = :html,
+  sitename ="MovingWeightedLeastSquares.jl",
+  authors = "Tung Anh Vu",
+  pages = Any[
+    "Home" => "index.md",
+    "Constructors" => "constructors.md",
+    "Approximation" => "approximation.md"
+  ]
+)
 
-deploydocs(deps = Deps.pip("mkdocs", "python-markdown-math"),
-           repo = "github.com/vutunganh/MovingWeightedLeastSquares.jl.git",
-           julia = "0.6",
-           osname = "linux")
+deploydocs(
+  repo = "github.com/vutunganh/MovingWeightedLeastSquares.jl.git",
+  target = "build",
+  julia = "0.6",
+  osname = "linux",
+  deps = nothing,
+  make = nothing
+)
