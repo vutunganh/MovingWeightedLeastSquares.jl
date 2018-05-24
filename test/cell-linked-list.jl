b80d7aa0c271ab1ea3415c04812e540e2445f51b
Tung Anh Vu <vu.tunganh96@gmail.com>
2018-05-24 11:05:10 +0200

    naming convention for tests

--- test/cell-linked-list.jl
+++ test/cell-linked-list.jl
@@ -3,19 +3,19 @@
   dataset = range * rand(2, 100) - range / 2
   cll = CellLinkedList(dataset, 3.)
 
-  testStatus = true
+  teststatus = true
   for c in 1:100
     pt = 1.1 * range * rand(2) - range * 1.1 / 2
     r = range / 3
-    cllRes = cllInrange(cll, pt, r)
-    naiveRes = [p for p in 1:size(dataset, 2) if norm(pt - dataset[:, p]) < r + 1e-9]
-    testStatus &= sort(cllRes) == sort(naiveRes)
-    if !testStatus
+    cllres = cll_inrange(cll, pt, r)
+    naiveres = [p for p in 1:size(dataset, 2) if norm(pt - dataset[:, p]) < r + 1e-9]
+    teststatus &= sort(cllres) == sort(naiveres)
+    if !teststatus
       @show dataset
       @show pt
       @show r
       break
     end
   end
-  @test testStatus == true
+  @test teststatus == true
 end
---
+++ test/single-array-input.jl
@@ -0,0 +1,27 @@
+@testset "single array input" begin
+  xs = collect(0:0.1:10)
+  fs = [sin(x) for x in xs]
+  data = hcat(xs, fs)
+
+  m = mwls_kd(data, 1, (d, e) -> exp(-d^2))
+  cll = mwls_cll(data, 1, (d, e) -> exp(-d^2))
+
+  teststatus = true
+  for p in 0:0.1:10
+    teststatus &= isapprox(m(p), sin(p), atol=0.1)
+  end
+  @test teststatus == true
+
+  teststatus = true
+  for p in 0:0.1:10
+    teststatus &= isapprox(cll(p), sin(p), atol=0.1)
+  end
+  @test teststatus == true
+
+  # not enough data for approximation, zero returned by default
+  @test isapprox(m(-0.9), 0, atol=0.1)
+  @test isapprox(cll(-0.9), 0, atol=0.1)
+  @test isapprox(m(10.9), 0, atol=0.1)
+  @test isapprox(cll(10.9), 0, atol=0.1)
+end
+
